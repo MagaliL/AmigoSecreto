e60714dd24fa8efa9c13b09fb3b10ef9b9aee97e
Magali Espinosa <magalizethe@gmail.com>
2025-09-17 10:14:16 -0600

    Se agregó la función para listar los nombres en pantalla y para sortearlos y que el resultado aparezca en pantalla

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let amigos = [];
 function agregarAmigo(){
     
     let amigoUsuario = document.getElementById('amigo').value;
-    console.log(amigoUsuario);
+    // console.log(amigoUsuario);
 
     if (amigoUsuario === '' ){
 
@@ -18,23 +18,66 @@ function agregarAmigo(){
 
             alert(`El amigo ${amigoUsuario} ya está en tu lista.`);
            
-       
 
         } else{
 
                 amigos.push(amigoUsuario);
                 console.log(amigos);
+                listaAmigos();
                 }
 
              limpiarcaja();    
         
-             }
-
-            
+             }       
 
 }
+// Esta función es para agregar elementos ingresados por el usuario a la lista declarada anteriormente.
+
 
 function limpiarcaja(){
     document.querySelector('#amigo').value = "";
     
 }
+
+function listaAmigos (){
+
+    let listaAmi = document.getElementById('listaAmigos');
+    // Limpiar la lista antes de agregar nuevos elementos
+     listaAmi.innerHTML = "";
+
+     for ( let i = 0; i<amigos.length ; i++ ){
+
+        let nuevoAmigo = document.createElement('li');
+        //Crear nuevo elemento li para cada amigo
+        nuevoAmigo.textContent = amigos[i];
+        //Recorre la lista amigos y lo agrega a nuevoAmigo
+        listaAmi.appendChild(nuevoAmigo);
+         // Agregar el nuevo <li> a la lista HTML
+     }
+
+
+}
+//Esta función imprime en pantalla la lista de amigos que el usario va ingresando
+
+function sortearAmigo(){
+
+    //Verificar que el arreglo de amigos tenga elementos
+    if (amigos.length == 0){
+
+        alert('No tienes has agregado amigos a tu lista.');
+    } else {
+         //Generar un índice aleatorio
+        let indiceRandom = Math.floor(Math.random()*amigos.length);
+        let amigoSorteado = document.getElementById('resultado');
+        amigoSorteado.innerHTML = ""; //limpiar contenido anterior
+
+        let resultadoText = document.createElement('H2');
+        resultadoText.textContent = `El amigo sorteado es ${amigos[indiceRandom]}`;
+
+        amigoSorteado.appendChild(resultadoText);
+        //mostrar el resultado en pantalla
+
+    }
+    
+}
+
