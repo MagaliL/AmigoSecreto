53f66b397e2333cb0ee985850b5286093a60a8e3
Magali Espinosa <magalizethe@gmail.com>
2025-09-16 23:44:28 -0600

    Se realizó la función agregar amigo en el que se recibe un nombre del usuario y se guarda en la variable amigoUsuario, después se verifica que la caja no esté vacía, que no se repita el nombre ingresado en el arreglo agregarAmigo y se muestra el arreglo en consola.

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []; 
+
+
+function agregarAmigo(){
+    
+    let amigoUsuario = document.getElementById('amigo').value;
+    console.log(amigoUsuario);
+
+    if (amigoUsuario === '' ){
+
+        alert('Por favor, ingrese un nombre');
+        return; 
+    } else {
+
+        if (amigos.includes(amigoUsuario)) {
+
+            alert(`El amigo ${amigoUsuario} ya está en tu lista.`);
+           
+       
+
+        } else{
+
+                amigos.push(amigoUsuario);
+                console.log(amigos);
+                }
+
+             limpiarcaja();    
+        
+             }
+
+            
+
+}
+
+function limpiarcaja(){
+    document.querySelector('#amigo').value = "";
+    
+}
